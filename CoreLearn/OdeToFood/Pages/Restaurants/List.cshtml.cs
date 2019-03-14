afe9a30a34c59db53ab9c519c2acbf2bf165aa06
wongomao <norman@wongomao.com>
2019-03-13 20:19:44 -0600

    add Message from appsettings

--- CoreLearn/OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ CoreLearn/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -4,14 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
+        public string Message { get; set; }
+        private readonly IConfiguration _configuration;
+
+        public ListModel(IConfiguration config)
+        {
+            _configuration = config;
+        }
+
         public void OnGet()
         {
-
+            Message = _configuration["Message"];
         }
     }
 }
