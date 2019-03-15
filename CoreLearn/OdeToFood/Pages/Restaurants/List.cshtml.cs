a1c002bf796573d9883bb5f920b382feedaeaf18
norman parker <nparker@avidxchange.com>
2019-03-15 07:33:38 -0600

    dependency injection - restaurants data

--- CoreLearn/OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ CoreLearn/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -5,22 +5,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
-        public string Message { get; set; }
         private readonly IConfiguration _configuration;
+        private readonly IRestaurantData _restaurantData;
 
-        public ListModel(IConfiguration config)
+        public string Message { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
         {
             _configuration = config;
+            _restaurantData = restaurantData;
         }
 
         public void OnGet()
         {
             Message = _configuration["Message"];
+            Restaurants = _restaurantData.GetAll();
         }
     }
 }
