a9cffe5360bb6f9c5e78dce1fa69974baab2d69c
Norman Parker <norman@wongomao.com>
2019-03-16 09:03:13 -0600

    start restaurant search

--- CoreLearn/OdeToFood.Data/IRestaurantData.cs
+++ CoreLearn/OdeToFood.Data/IRestaurantData.cs
@@ -6,7 +6,7 @@ namespace OdeToFood.Data
 {
     public interface IRestaurantData
     {
-        IEnumerable<Restaurant> GetAll();
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
     }
 
     public class InMemoryRestaurantData : IRestaurantData
@@ -23,9 +23,10 @@ namespace OdeToFood.Data
             };
         }
 
-        public IEnumerable<Restaurant> GetAll()
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
         {
             return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
                    orderby r.Name
                    select r;
         }
