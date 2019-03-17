342cf9585f5484951ee4aff6c7f091c90b553e56
Norman Parker <norman@wongomao.com>
2019-03-17 08:06:18 -0600

    start model binding

--- CoreLearn/OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ CoreLearn/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -17,6 +17,8 @@ namespace OdeToFood.Pages.Restaurants
 
         public string Message { get; set; }
         public IEnumerable<Restaurant> Restaurants { get; set; }
+        [BindProperty(SupportsGet =true)]
+        public string SearchTerm { get; set; }
 
         public ListModel(IConfiguration config, IRestaurantData restaurantData)
         {
@@ -27,7 +29,7 @@ namespace OdeToFood.Pages.Restaurants
         public void OnGet()
         {
             Message = _configuration["Message"];
-            Restaurants = _restaurantData.GetAll();
+            Restaurants = _restaurantData.GetRestaurantsByName(SearchTerm);
         }
     }
 }
