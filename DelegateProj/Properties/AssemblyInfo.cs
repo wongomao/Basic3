6693f72267c12553b723b0fc7d08a093af332466
wongomao <norman@wongomao.com>
2020-05-21 07:26:17 -0600

    adding threading basics

---
+++ DelegateProj/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DelegateProj")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DelegateProj")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5523fcee-84e4-4f2a-a9fd-3715882526e0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DelegateProj/UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DelegateProj
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public void DoWork()
+        {
+            Debug.WriteLine($"inside DoWork() thread id = {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        delegate void CallableObj();
+
+        [TestMethod]
+        public void TestMethod01()
+        {
+            DoWork();
+        }
+
+        [TestMethod]
+        public void TestMethod02()
+        {
+            CallableObj callableObj = new CallableObj(DoWork);
+            callableObj.Invoke();
+        }
+
+        [TestMethod]
+        public void TestMethod03()
+        {
+            Debug.WriteLine($"main thread {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+            CallableObj callableObj = new CallableObj(DoWork);
+            IAsyncResult asyncResult = callableObj.BeginInvoke(null, null);
+            callableObj.EndInvoke(asyncResult);
+        }
+
+        private static void MyCallbackFn(IAsyncResult asyncResult)
+        {
+            Debug.WriteLine($"inside callback thread id = {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+            // find the delegate object from asyncResult->asyncState
+            var delegateObj = asyncResult.AsyncState as CallableObj;
+            // finish the invocation of thread
+            delegateObj.EndInvoke(asyncResult);
+        }
+
+        [TestMethod]
+        public void TestMethod04()
+        {
+            Debug.WriteLine($"main thread id = {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+            CallableObj callableObj = new CallableObj(DoWork);
+            // make a callback object
+            AsyncCallback asyncCallback = new AsyncCallback(MyCallbackFn);
+            IAsyncResult asyncResult = callableObj.BeginInvoke(asyncCallback, callableObj);
+
+            // notice that the BeginInvoke returns immediately in this thread - now we must wait
+            //  for the thread to finish
+            // just call async-wait for handle for now. other circumstances would wait on data or something
+            asyncResult.AsyncWaitHandle.WaitOne(); // blocks self and waits for other thread
+        }
+    }
+}
