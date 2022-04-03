8123090cc29dee9408871eb31298480c6df7f987
Norman Parker <NParker@avidxchange.com>
2022-04-03 08:29:35 -0600

    update to framework 4.8

---
+++ NullableRefsSample/Model.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace NullableRefsSample
+{
+    public class BlogPost
+    {
+        public string? Title { get; set; }
+        public List<Comment>? Comments { get; set; }
+    }
+
+    public class Comment
+    {
+        public string? Body { get; set; }
+        public Author? PostedBy { get; set; }
+    }
+
+    public class Author
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+    }
+
+}
